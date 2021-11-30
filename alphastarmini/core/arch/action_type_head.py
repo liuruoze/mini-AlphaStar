* improve target_unit_head and fix a big loss in units calculation;

alphastarmini/core/arch/action_type_head.py
@@ -174,7 +174,7 @@ class ActionTypeHead(nn.Module):
             print('get_cuda_device:', get_cuda_device) if debug else None
 
         # change action_type to one_hot version
-        action_type_one_hot = L.to_one_hot(action_type, self.max_action_num)
+        action_type_one_hot = L.tensor_one_hot(action_type, self.max_action_num)
         print('action_type_one_hot.shape:', action_type_one_hot.shape) if debug else None
         # to make the dim of delay_one_hot as delay
         action_type_one_hot = action_type_one_hot.squeeze(-2)

alphastarmini/core/arch/arch_model.py
@@ -100,7 +100,7 @@ class ArchModel(nn.Module):
                 return_baseline=False, multi_gpu_supvised_learning=False,
                 use_scatter_map=True):
         # shapes of embedded_entity, embedded_spatial, embedded_scalar are all [batch_size x embedded_size]
-        entity_embeddings, embedded_entity, entity_num = self.entity_encoder(state.entity_state)   
+        entity_embeddings, embedded_entity, entity_nums = self.entity_encoder(state.entity_state)   
 
         if AHP.scatter_channels:
             map_skip, embedded_spatial = self.spatial_encoder(state.map_state, entity_embeddings)
@@ -121,10 +121,10 @@ class ArchModel(nn.Module):
         units_logits, units, autoregressive_embedding, select_units_num = self.selected_units_head(autoregressive_embedding, 
                                                                                                    action_type, 
                                                                                                    entity_embeddings, 
-                                                                                                   entity_num)
+                                                                                                   entity_nums)
 
         target_unit_logits, target_unit = self.target_unit_head(autoregressive_embedding, 
-                                                                action_type, entity_embeddings)
+                                                                action_type, entity_embeddings, entity_nums)
         target_location_logits, target_location = self.location_head(autoregressive_embedding, action_type, map_skip)
 
         action_logits = ArgsActionLogits(action_type=action_type_logits, delay=delay_logits, queue=queue_logits,
@@ -134,7 +134,7 @@ class ArchModel(nn.Module):
                             units=units, target_unit=target_unit, target_location=target_location)
 
         if multi_gpu_supvised_learning:
-            return action_type, units, target_location, action_type_logits, delay_logits, queue_logits, \
+            return action_type, units, target_unit, target_location, action_type_logits, delay_logits, queue_logits, \
                 units_logits, target_unit_logits, target_location_logits, select_units_num
 
         if return_logits:

alphastarmini/core/arch/target_unit_head.py
@@ -54,22 +54,22 @@ class TargetUnitHead(nn.Module):
 
         self.softmax = nn.Softmax(dim=-1)
 
-    def forward(self, autoregressive_embedding, action_type, entity_embeddings):
+    def forward(self, autoregressive_embedding, action_type, entity_embeddings, entity_num):
         '''
         Inputs:
             autoregressive_embedding: [batch_size x autoregressive_embedding_size]
             action_type: [batch_size x 1]
             entity_embeddings: [batch_size x entity_size x embedding_size]
+            entity_num: [batch_size]
         Output:
             target_unit_logits: [batch_size x max_selected x entity_size]
             target_unit: [batch_size x max_selected x 1]
         '''
 
         batch_size = entity_embeddings.shape[0]
-        assert autoregressive_embedding.shape[0] == action_type.shape[0]
-        assert autoregressive_embedding.shape[0] == entity_embeddings.shape[0]
+
         # entity_embeddings shape is [batch_size x entity_size x embedding_size]
-        entity_size = entity_embeddings.shape[-2]
+        entity_size = entity_embeddings.shape[1]
 
         # `func_embed` is computed the same as in the Selected Units head, 
         # and used in the same way for the query (added to the output of the `autoregressive_embedding` 
@@ -79,12 +79,21 @@ class TargetUnitHead(nn.Module):
         device = next(self.parameters()).device
         unit_types_one_hot = unit_types_one_hot.to(device)
 
-        assert unit_types_one_hot.shape[-1] == self.max_number_of_unit_types
         # unit_types_mask shape: [batch_size x self.max_number_of_unit_types]
         the_func_embed = F.relu(self.func_embed(unit_types_one_hot))
+
         # the_func_embed shape: [batch_size x 256]
         print("the_func_embed:", the_func_embed) if debug else None
         print("the_func_embed.shape:", the_func_embed.shape) if debug else None
+
+        # generate the length mask for all entities
+        mask = torch.arange(entity_size, device=device).float()
+        mask = mask.repeat(batch_size, 1)
+
+        # mask: [batch_size, entity_size]
+        mask = mask < entity_num.unsqueeze(dim=1)
+        print("mask:", mask) if debug else None
+        print("mask.shape:", mask.shape) if debug else None
 
         # Because we mostly target one unit, we don't need a mask.
 
@@ -93,6 +102,7 @@ class TargetUnitHead(nn.Module):
         # same way as in the Selected Units head to get `target_unit_logits`.
         # input : [batch_size x entity_size x embedding_size]
         key = self.conv_1(entity_embeddings.transpose(-1, -2)).transpose(-1, -2)
+
         # output : [batch_size x entity_size x key_size], note key_size = 32
         print("key:", key) if debug else None
         print("key.shape:", key.shape) if debug else None
@@ -104,52 +114,40 @@ class TargetUnitHead(nn.Module):
         # note: repeated for selecting up to one unit
         max_selected = self.max_selected
         for i in range(max_selected):
-            x = self.fc_1(autoregressive_embedding)
-            print("x:", x) if debug else None
-            print("x.shape:", x.shape) if debug else None
-
-            assert the_func_embed.shape == x.shape
-            z_1 = the_func_embed + x
-            print("z_1:", z_1) if debug else None
-            print("z_1.shape:", z_1.shape) if debug else None
-
-            z_2 = self.fc_2(z_1)
-            print("z_2:", z_2) if debug else None
-            print("z_2.shape:", z_2.shape) if debug else None
-            z_2 = z_2.unsqueeze(1)
-
-            # The result is fed into a LSTM with size 32 and zero initial state to get a query.
-            if i == 0:
-                query, hidden = self.small_lstm(z_2)
-            else:
-                query, hidden = self.small_lstm(z_2, hidden)
-            print("query:", query) if debug else None
-            print("query.shape:", query.shape) if debug else None
-
             # AlphaStar: The query is then passed through a ReLU and a linear of size 32, 
-            # AlphaStar: and the query is applied to the keys which are created the same way as in 
-            # AlphaStar: the Selected Units head to get `target_unit_logits`.
+            # and the query is applied to the keys which are created the same way as in 
+            # the Selected Units head to get `target_unit_logits`.
+            x = self.fc_1(autoregressive_embedding)
+            x = the_func_embed + x
+            query = self.fc_2(x).unsqueeze(1)
+
+            # we don't need a lstm now
+            # query, hidden = self.small_lstm(x)
 
             # below is matrix multiply
             # key_shape: [batch_size x entity_size x key_size], note key_size = 32
             # query_shape: [batch_size x seq_len x hidden_size], note hidden_size is also 32, seq_len = 1
             y = torch.bmm(key, query.transpose(-1, -2))
-            print("y:", y) if debug else None
-            print("y.shape:", y.shape) if debug else None
-            y = y.squeeze(-1)
-            # y shape: [batch_size x entity_size]
 
-            target_unit_logits = y.div(self.temperature)
+            # new y shape: [batch_size x entity_size]
+            y = y.squeeze(-1)
+
+            # fill the entity which should be selected a very large negetive value 
+            y = y.masked_fill(~mask, -1e9)
+
             # target_unit_logits shape: [batch_size x entity_size]
+            target_unit_logits = y.div(self.temperature)
             print("target_unit_logits:", target_unit_logits) if debug else None
             print("target_unit_logits.shape:", target_unit_logits.shape) if debug else None
 
-            target_unit_probs = self.softmax(target_unit_logits)
             # target_unit_probs shape: [batch_size x entity_size]
+            target_unit_probs = self.softmax(target_unit_logits)
+            print("target_unit_probs:", target_unit_probs) if debug else None
+            print("target_unit_probs.shape:", target_unit_probs.shape) if debug else None
 
             # AlphaStar: `target_unit` is sampled from `target_unit_logits` using a multinomial with temperature 0.8.
-            target_unit_id = torch.multinomial(target_unit_probs, 1)
             # target_unit_id shape: [batch_size x 1]
+            target_unit_id = torch.multinomial(target_unit_probs, 1)    
             print("target_unit_id:", target_unit_id) if debug else None
             print("target_unit_id.shape:", target_unit_id.shape) if debug else None
 
@@ -163,32 +161,40 @@ class TargetUnitHead(nn.Module):
             # target location), it does not return `autoregressive_embedding`.
 
         # note: we only select one unit, so return the first one
-        target_unit_logits = torch.cat(target_unit_logits_list, dim=1)
         # target_unit_logits: [batch_size x max_selected x entity_size]
-        target_unit = torch.cat(target_unit_list, dim=1)
+        target_unit_logits_all = torch.cat(target_unit_logits_list, dim=1)
+
         # target_units: [batch_size x max_selected x 1]
+        target_unit_all = torch.cat(target_unit_list, dim=1)
 
         # AlphaStar: If `action_type` does not involve targetting units, this head is ignored.
-        target_unit_mask = L.action_involve_targeting_units_mask(action_type)
         # target_unit_mask: [batch_size x 1]
+        target_unit_mask = L.action_involve_targeting_units_mask(action_type).bool()
         print("target_unit_mask:", target_unit_mask) if debug else None
-
-        print("target_unit_logits.shape:", target_unit_logits.shape) if debug else None
         print("target_unit_mask.shape:", target_unit_mask.shape) if debug else None
-        target_unit_logits = target_unit_logits * target_unit_mask.float().unsqueeze(-1)
-        print("target_unit.shape:", target_unit.shape) if debug else None
-        target_unit = target_unit * target_unit_mask.long().unsqueeze(-1)
 
-        return target_unit_logits, target_unit
+        no_target_unit_mask = ~target_unit_mask.squeeze(dim=1)
+        print("no_target_unit_mask:", no_target_unit_mask) if debug else None
+
+        target_unit_logits_all[no_target_unit_mask] = 0.  # a magic number
+        target_unit_all[no_target_unit_mask] = entity_size - 1  # None index, the same as -1
+
+        print("target_unit_logits_all:", target_unit_logits_all) if debug else None
+        print("target_unit_all:", target_unit_all) if debug else None
+
+        return target_unit_logits_all, target_unit_all
 
 
 def test():
     action_type_sample = 352  # func: 352/Effect_WidowMineAttack_unit (1/queued [2]; 2/unit_tags [512]; 3/target_unit_tag [512])
 
-    batch_size = 2
+    batch_size = 4
     autoregressive_embedding = torch.randn(batch_size, AHP.autoregressive_embedding_size)
-    action_type = torch.randint(low=0, high=SFS.available_actions, size=(batch_size, 1))
+    #action_type = torch.randint(low=0, high=SFS.available_actions, size=(batch_size, 1))
+    action_type = torch.tensor([[0], [1], [168], [352]])
+
     entity_embeddings = torch.randn(batch_size, AHP.max_entities, AHP.entity_embedding_size)
+    entity_nums = torch.tensor([1, 2, 3, 12])
 
     target_units_head = TargetUnitHead()
 
@@ -196,7 +202,7 @@ def test():
     print("autoregressive_embedding.shape:", autoregressive_embedding.shape) if debug else None
 
     target_unit_logits, target_unit = \
-        target_units_head.forward(autoregressive_embedding, action_type, entity_embeddings)
+        target_units_head.forward(autoregressive_embedding, action_type, entity_embeddings, entity_nums)
 
     if target_unit_logits is not None:
         print("target_unit_logits:", target_unit_logits) if debug else None

alphastarmini/core/sl/sl_loss_multi_gpu.py
@@ -26,15 +26,57 @@ debug = False
 # due to CrossEntropyLoss only accepts loss with lables.shape = [N]
 # we define a loss accept soft_target, which label.shape = [N, C]
 # for some rows, it should not be added to loss, so we also need a mask one
-def cross_entropy(soft_targets, pred, mask=None):
+def cross_entropy(soft_targets, pred, mask=None, debug=False, outlier_remove=True):
     # class is always in the last dim
     logsoftmax = nn.LogSoftmax(dim=-1)
     x_1 = - soft_targets * logsoftmax(pred)
+
+    if debug:
+        for i, (t, p) in enumerate(zip(soft_targets, logsoftmax(pred))):
+            # print('t', t)
+            # print('t.shape', t.shape)
+            # print('p', p)
+            # print('p.shape', p.shape)
+            value = - t * logsoftmax(p)
+            # print('value', value)
+            # print('value.shape', value.shape)
+            m = mask[i].item()
+            if value.sum() > 1e6 and m != 0:
+                print('find value large than 1e6')
+                print('t', t)
+                z = torch.nonzero(t, as_tuple=True)[-1]
+                print('z', z)
+                idx = z.item()
+                print('p', p)
+                q = p[idx]
+                print('q', q)
+                print('value', value)
+                print('m', m)
+                stop()
+
+    print('x_1:', x_1) if debug else None
+    print('x_1.shape:', x_1.shape) if debug else None
+
     x_2 = torch.sum(x_1, -1)
+    print('x_2:', x_2) if debug else None
+    print('x_2.shape:', x_2.shape) if debug else None
+
     # This mask is for each item's mask
     if mask is not None:
         x_2 = x_2 * mask
+
+        if outlier_remove:
+            outlier_mask = (x_2 >= 1e6)
+            x_2 = x_2 * ~outlier_mask
+        else:
+            outlier_mask = (x_2 >= 1e6)
+            if outlier_mask.any() > 0:
+                stop()
+
     x_4 = torch.mean(x_2)
+    print('x_4:', x_4) if debug else None
+    print('x_4.shape:', x_4.shape) if debug else None
+
     return x_4
 
 
@@ -96,8 +138,7 @@ def get_sl_loss(traj_batch, model, use_mask=True, use_eval=False):
 
 def get_sl_loss_for_tensor(features, labels, model, decrease_smart_opertaion=False,
                            return_important=False, only_consider_small=False,
-                           include_selected_units_accuracy=False,
-                           include_location_accuracy=False):
+                           ):
 
     criterion = cross_entropy
 
@@ -132,7 +173,7 @@ def get_sl_loss_for_tensor(features, labels, model, decrease_smart_opertaion=Fal
     # we can't make them all into as a list or into ArgsActionLogits
     # if we do that, the pytorch DDP will cause a runtime error, just like the loss don't include all parameters
     # This error is strange, so we choose to use a specific loss writing schema for multi-gpu calculation.
-    action_pred, units, target_location, action_type_logits, \
+    action_pred, units, target_unit, target_location, action_type_logits, \
         delay_logits, queue_logits, \
         units_logits, target_unit_logits, \
         target_location_logits, select_units_num = model.forward(state, 
@@ -148,18 +189,20 @@ def get_sl_loss_for_tensor(features, labels, model, decrease_smart_opertaion=Fal
                                                              target_unit_logits, target_location_logits, 
                                                              select_units_num, criterion, device, 
                                                              decrease_smart_opertaion=decrease_smart_opertaion,
-                                                             only_consider_small=only_consider_small)
-    acc_num_list = SU.get_accuracy(action_gt.action_type, action_pred, 
-                                   device, return_important=return_important)
-    location_acc = SU.get_location_accuracy(action_gt.target_location, target_location, device, 
-                                            return_important=return_important)
-    selected_acc = SU.get_selected_units_accuracy(action_gt.units, units, select_units_num,
-                                                  device, return_important=return_important) 
-    if include_location_accuracy:
-        acc_num_list.extend(location_acc)
+                                                             only_consider_small=only_consider_small)  
+    acc_num_list, action_equal_mask = SU.get_accuracy(action_gt.action_type, action_pred, 
+                                                      device, return_important=return_important)
 
-    if include_selected_units_accuracy:
-        acc_num_list.extend(selected_acc)
+    location_acc = SU.get_location_accuracy(action_gt.target_location, target_location, action_equal_mask, device, 
+                                            strict_comparsion=True)
+    selected_acc = SU.get_selected_units_accuracy(action_gt.units, units, select_units_num, action_equal_mask,
+                                                  device, strict_comparsion=True)
+    targeted_acc = SU.get_target_unit_accuracy(action_gt.target_unit, target_unit, action_equal_mask,
+                                               device, strict_comparsion=True)
+
+    acc_num_list.extend(location_acc)
+    acc_num_list.extend(selected_acc)
+    acc_num_list.extend(targeted_acc)
 
     print('loss', loss) if debug else None
 
@@ -170,7 +213,8 @@ def get_masked_classify_loss_for_multi_gpu(action_gt, action_pred, action_type, 
                                            target_unit, target_location, select_units_num,
                                            criterion, device, 
                                            decrease_smart_opertaion=False,
-                                           only_consider_small=False):
+                                           only_consider_small=False,
+                                           strict_comparsion=True, remove_none=True):
     loss = 0.
 
     # consider using move camera weight
@@ -221,14 +265,17 @@ def get_masked_classify_loss_for_multi_gpu(action_gt, action_pred, action_type, 
     print('all_units_mask', all_units_mask) if debug else None
 
     # TODO: change to a proporate calculation of selected units
-    units_loss = criterion(action_gt.units.reshape(-1, units_size), units.reshape(-1, units_size), mask=all_units_mask)
+    selected_units_weight = 10.
+    units_loss = selected_units_weight * criterion(action_gt.units.reshape(-1, units_size), units.reshape(-1, units_size), mask=all_units_mask, debug=False)
     loss += units_loss
 
-    target_unit_loss = criterion(action_gt.target_unit.squeeze(-2), target_unit.squeeze(-2), mask=mask_tensor[:, 4].reshape(-1))
+    target_unit_weight = 5.
+    target_unit_loss = target_unit_weight * criterion(action_gt.target_unit.squeeze(-2), target_unit.squeeze(-2), 
+                                                      mask=mask_tensor[:, 4].reshape(-1), debug=False, outlier_remove=True)
     loss += target_unit_loss
 
     batch_size = action_gt.target_location.shape[0]
-    location_weight = 10.
+    location_weight = 1.
     target_location_loss = location_weight * criterion(action_gt.target_location.reshape(batch_size, -1),
                                                        target_location.reshape(batch_size, -1), mask=mask_tensor[:, 5].reshape(-1))
     loss += target_location_loss

alphastarmini/core/sl/sl_utils.py
@@ -141,7 +141,7 @@ def get_one_way_mask_in_SL(action_type_gt, device):
     return mask_tensor
 
 
-def get_two_way_mask_in_SL(action_type_gt, action_pred, device, strict_comparsion=False):
+def get_two_way_mask_in_SL(action_type_gt, action_pred, device, strict_comparsion=True):
     # consider the ground truth and the predicted
     ground_truth_raw_action_id = torch.nonzero(action_type_gt, as_tuple=True)[-1]
     action_pred = action_pred.reshape(-1)
@@ -259,93 +259,133 @@ def get_move_camera_weight_in_SL(action_type_gt, action_pred, device,
     return mask_tensor
 
 
-def get_selected_units_accuracy(ground_truth, predict, select_units_num, device, return_important=False):
-    print('ground_truth.shape', ground_truth.shape) if debug else None
-    batch_size = ground_truth.shape[0]
+def get_selected_units_accuracy(ground_truth, predict, select_units_num, action_equal_mask, 
+                                device, strict_comparsion=True, use_strict_order=False):
+    all_num, correct_num, gt_num, pred_num = 0, 0, 1, 0
+    if strict_comparsion:
+        action_equal_index = action_equal_mask.nonzero(as_tuple=True)[0]
+        ground_truth = ground_truth[action_equal_index]
+        predict = predict[action_equal_index]
 
-    all_num, correct_num, gt_num, pred_num = 0, 0, 0, 0
+    if ground_truth.shape[0] > 0:  
+        size = ground_truth.shape[0]
+        NONE_INDEX = AHP.max_entities - 1
 
-    for i in range(batch_size):
-        ground_truth_sample = ground_truth[i]
-        ground_truth_new = torch.nonzero(ground_truth_sample, as_tuple=True)[-1]
-        ground_truth_new = ground_truth_new.to(device)
-        print('ground_truth units', ground_truth_new) if debug else None
+        for i in range(size):
+            ground_truth_sample = ground_truth[i]
+            ground_truth_new = torch.nonzero(ground_truth_sample, as_tuple=True)[-1]
+            ground_truth_new = ground_truth_new.cpu().detach().numpy().tolist()
 
-        predict_sample = predict[i].reshape(-1)
-        print('predict_sample units', predict_sample) if debug else None
+            print('ground_truth units', ground_truth_new) if debug else None
 
-        select_units_num_sample = select_units_num[i].item()
-        print('select_units_num_sample units', select_units_num_sample) if debug else None
+            predict_sample = predict[i].reshape(-1)
+            print('predict_sample units', predict_sample) if debug else None
 
-        for j in range(select_units_num_sample):
-            pred = predict_sample[j].item()
-            gt = ground_truth_new[j].item()
+            select_units_num_sample = select_units_num[i].item()
+            print('select_units_num_sample units', select_units_num_sample) if debug else None
 
-            if gt != AHP.max_entities - 1:
-                gt_num += 1
-                if pred == gt:
-                    correct_num += 1
+            for j in range(select_units_num_sample):
+                pred = predict_sample[j].item()
+                gt = ground_truth_new[j]
+                if gt != NONE_INDEX:  # the last index is the None index
+                    gt_num += 1
+                if use_strict_order:
+                    if pred == gt and pred != NONE_INDEX:
+                        correct_num += 1
+                else:
+                    if pred in ground_truth_new and pred != NONE_INDEX:
+                        correct_num += 1
+                pred_num += 1
 
-            pred_num += 1
+            all_num += AHP.max_selected
 
-        all_num += AHP.max_selected
-
-    selected_units_accuracy = correct_num / (gt_num + 1e-9)
-    selected_units_coverage = pred_num / (gt_num + 1e-9)
-
-    print([correct_num, gt_num, pred_num, all_num])
+    print('get_selected_units_accuracy', [correct_num, gt_num, pred_num, all_num])
 
     return [correct_num, gt_num, pred_num, all_num]
 
 
-def get_location_accuracy(ground_truth, predict, device, return_important=False):
-    accuracy = 0.
+def get_target_unit_accuracy(ground_truth, predict, action_equal_mask, device, 
+                             strict_comparsion=True, remove_none=True):
+    right_num, all_num = 0, 0
 
-    # print('location ground_truth', ground_truth)
-    # print('location ground_truth.shape', ground_truth.shape)
+    if strict_comparsion:
+        action_equal_index = action_equal_mask.nonzero(as_tuple=True)[0]
+        ground_truth = ground_truth[action_equal_index]
+        predict = predict[action_equal_index]
 
-    ground_truth = ground_truth.reshape(ground_truth.shape[0], -1)
-    ground_truth_new = torch.nonzero(ground_truth, as_tuple=True)[-1]
-    ground_truth_new = ground_truth_new.to(device)
-    print('ground_truth location', ground_truth_new) if debug else None
+    if ground_truth.shape[0] > 0:  
+        print('ground_truth target_unit', ground_truth)
+        ground_truth_new = torch.nonzero(ground_truth, as_tuple=True)[-1]
+        ground_truth_new = ground_truth_new.to(device)
+        print('ground_truth_new target_unit', ground_truth_new) if debug else None
 
-    output_map_size = SCHP.world_size
+        predict_new = predict.reshape(-1)
+        print('predict_new target_unit', predict_new)
 
+        NONE_ID = AHP.max_entities - 1
+        if remove_none:
+            effect_index = (ground_truth_new != NONE_ID).nonzero(as_tuple=True)[0]
+            ground_truth_new = ground_truth_new[effect_index]
+            predict_new = predict_new[effect_index]
+
+        right_num, all_num = get_right_and_all_num(ground_truth_new, predict_new)
+
+    print('get_target_unit_accuracy', [right_num, all_num])
+
+    return [right_num, all_num]
+
+
+def get_location_accuracy(ground_truth, predict, action_equal_mask, device, strict_comparsion=True):
     all_nums = ground_truth.shape[0]
+
     effect_nums = 0  # when the location argument applied both in ground_truth and predict
     correct_nums = 0
     distance_loss = 0.
 
-    for i, idx in enumerate(ground_truth_new):
-        row_number = idx // output_map_size
-        col_number = idx - output_map_size * row_number
+    if strict_comparsion:
+        action_equal_index = action_equal_mask.nonzero(as_tuple=True)[0]
+        ground_truth = ground_truth[action_equal_index]
+        predict = predict[action_equal_index]
 
-        gt_location_y = row_number
-        gt_location_x = col_number
-        print("gt_location_y, gt_location_x", gt_location_y, gt_location_x) if debug else None
+    if ground_truth.shape[0] > 0:    
 
-        [predict_x, predict_y] = predict[i] 
-        print("predict_x, predict_y", predict_x, predict_y) if debug else None
+        ground_truth = ground_truth.reshape(ground_truth.shape[0], -1)
+        ground_truth_new = torch.nonzero(ground_truth, as_tuple=True)[-1]
+        ground_truth_new = ground_truth_new.to(device)
+        print('ground_truth location', ground_truth_new) if debug else None
 
-        x_diff_square = (predict_x.item() - gt_location_x.item()) ** 2
-        y_diff_square = (predict_y.item() - gt_location_y.item()) ** 2
+        output_map_size = SCHP.world_size
 
-        print('x_diff_square', x_diff_square) if debug else None
-        print('y_diff_square', y_diff_square) if debug else None
+        for i, idx in enumerate(ground_truth_new):
+            row_number = idx // output_map_size
+            col_number = idx - output_map_size * row_number
 
-        # pos(0, 0) isconsidered a flag meaning this arugment is not applied for this action;
-        # e.g., we hardly will choose or see a point of pos(0, 0)
-        if not (gt_location_y.item() == 0 and gt_location_x.item() == 0):
-            if not (predict_x.item() == 0 and predict_y.item() == 0):
-                effect_nums += 1
+            gt_location_y = row_number
+            gt_location_x = col_number
+            print("gt_location_y, gt_location_x", gt_location_y, gt_location_x) if debug else None
 
-                diff_square = x_diff_square + y_diff_square
-                distance_loss += diff_square
+            [predict_x, predict_y] = predict[i] 
+            print("predict_x, predict_y", predict_x, predict_y) if debug else None
 
-                if diff_square == 0:
-                    correct_nums += 1
+            x_diff_square = (predict_x.item() - gt_location_x.item()) ** 2
+            y_diff_square = (predict_y.item() - gt_location_y.item()) ** 2
 
-    print([correct_nums, effect_nums, all_nums, distance_loss]) if debug else None
+            print('x_diff_square', x_diff_square) if debug else None
+            print('y_diff_square', y_diff_square) if debug else None
+
+            # pos(output_map_size-1, output_map_size-1) isconsidered a flag meaning this arugment is not applied for this action;
+            # e.g., we hardly will choose or see a point of pos(output_map_size-1, output_map_size-1)
+            if not (gt_location_y.item() == output_map_size - 1 and gt_location_x.item() == output_map_size - 1):  # the last index is the None index
+                if not (predict_x.item() == 0 and predict_y.item() == 0):
+                    effect_nums += 1
+
+                    diff_square = x_diff_square + y_diff_square
+                    distance_loss += diff_square
+
+                    if diff_square == 0:
+                        correct_nums += 1
+
+    print('get_location_accuracy', [correct_nums, effect_nums, all_nums, distance_loss]) if debug else None
 
     return [correct_nums, effect_nums, all_nums, distance_loss]
 
@@ -359,6 +399,9 @@ def get_accuracy(ground_truth, predict, device, return_important=False):
 
     predict_new = predict.reshape(-1)
     print('predict_new', predict_new) if debug else None  
+
+    # shape: [batch_size]
+    action_equal_mask = (ground_truth_new == predict_new)
 
     # calculate how many move_camera? the id is 168 in raw_action
     MOVE_CAMERA_ID = 168
@@ -398,11 +441,10 @@ def get_accuracy(ground_truth, predict, device, return_important=False):
     short_important_predict_new = predict_new[short_important_index]
     short_important_right_num, short_important_all_num = get_right_and_all_num(short_important_ground_truth_new, short_important_predict_new)
 
-    if return_important:
-        return [right_num, all_num, camera_right_num, camera_all_num,
-                non_camera_right_num, non_camera_all_num, short_important_right_num, short_important_all_num]
+    acc_list = [right_num, all_num, camera_right_num, camera_all_num, non_camera_right_num, 
+                non_camera_all_num, short_important_right_num, short_important_all_num]
 
-    return [right_num, all_num, camera_right_num, camera_all_num, non_camera_right_num, non_camera_all_num]
+    return acc_list, action_equal_mask
 
 
 def get_right_and_all_num(gt, pred):