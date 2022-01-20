* fix some units loss problem in RL calculation which may make great loss;

alphastarmini/core/arch/arch_model.py
@@ -238,7 +238,7 @@ class ArchModel(nn.Module):
 
     def mimic_forward(self, state, gt_action, gt_select_units_num, 
                       gt_is_one_hot=True, multi_gpu_supvised_learning=False, batch_size=None, sequence_length=None, hidden_state=None,
-                      baseline_state=None, baseline_opponent_state=None):
+                      baseline_state=None, baseline_opponent_state=None, show=False):
         '''
         # inspired by the DI-star project
         # injected the args of ground truth into the forward calculation
@@ -322,12 +322,19 @@ class ArchModel(nn.Module):
         queue_logits, _, autoregressive_embedding = self.queue_head(autoregressive_embedding, gt_action_type, embedded_entity, gt_queue)
 
         # selected_units_head is special, we use forward_sl function
+        print('gt_units', gt_units) if show else None
+        print('gt_units.shape', gt_units.shape) if show else None
+
         units_logits, units, autoregressive_embedding, select_units_num = self.selected_units_head.mimic_forward(autoregressive_embedding, 
                                                                                                                  gt_action_type, 
                                                                                                                  entity_embeddings, 
                                                                                                                  entity_nums,
                                                                                                                  gt_units,
-                                                                                                                 gt_select_units_num)
+                                                                                                                 gt_select_units_num,
+                                                                                                                 show=show)
+
+        print('units_logits', units_logits) if show else None
+        print('units_logits.shape', units_logits.shape) if show else None
 
         target_unit_logits, target_unit = self.target_unit_head(autoregressive_embedding, 
                                                                 gt_action_type, entity_embeddings, entity_nums, gt_target_unit)

alphastarmini/core/arch/selected_units_head.py
@@ -110,15 +110,11 @@ class SelectedUnitsHead(nn.Module):
 
         # mask: [batch_size, entity_size]
         mask = mask < added_entity_num.unsqueeze(dim=1)
-        print("mask:", mask) if debug else None
-        print("mask.shape:", mask.shape) if debug else None
-
         assert mask.dtype == torch.bool
 
         # AlphaStar: It then computes a key corresponding to each entity by feeding `entity_embeddings`
         # through a 1D convolution with 32 channels and kernel size 1,
         # and creates a new variable corresponding to ending unit selection.
-
         # input: [batch_size x entity_size x embedding_size]
         # output: [batch_size x entity_size x key_size], note key_size = 32
         key = self.conv_1(entity_embeddings.transpose(-1, -2)).transpose(-1, -2)
@@ -150,8 +146,6 @@ class SelectedUnitsHead(nn.Module):
         # calculate the average of keys (consider the entity_num)
         key_mask = mask.unsqueeze(dim=2).repeat(1, 1, key.shape[-1])
         key_avg = torch.sum(key * key_mask, dim=1) / entity_num.reshape(batch_size, 1)
-        print("key_avg:", key_avg) if debug else None
-        print("key_avg.shape:", key_avg.shape) if debug else None
 
         # creates a new variable corresponding to ending unit selection.
         # QUESTION: how to do that?
@@ -173,68 +167,30 @@ class SelectedUnitsHead(nn.Module):
 
         # AlphaStar: repeated for selecting up to 64 units
         for i in range(self.max_selected):
-
-            # in the second selection, we can select the EOF
             if i == 1:
-                mask[torch.arange(batch_size), end_index] = True
+                mask[torch.arange(batch_size), end_index] = True  # in the second selection, we can select the EOF
 
-            # AlphaStar: the network passes `autoregressive_embedding` through a linear of size 256,
-            # autoregressive_embedding shape: [batch_size x autoregressive_embedding_size]
-            # x shape: [batch_size x 256]
             x = self.fc_1(autoregressive_embedding)
-
-            # AlphaStar: adds `func_embed`, and passes the combination through a ReLU and a linear of size 32.
-            # x shape: [batch_size x seq_len x 32], note seq_len = 1
             x = self.fc_2(F.relu(x + the_func_embed)).unsqueeze(dim=1)
 
-            # AlphaStar: The result is fed into a LSTM with size 32 and zero initial state to get a query.
             query, hidden = self.small_lstm(x, hidden)
-
-            # AlphaStar: The entity keys are multiplied by the query, and are sampled using the mask and temperature 0.8 
-            # to decide which entity to select.
-            # key_shape: [batch_size x entity_size x key_size], note key_size = 32
-            # query_shape: [batch_size x seq_len x hidden_size], note hidden_size is also 32, seq_len = 1
-            # y shape: [batch_size x entity_size]
             y = torch.sum(query * key, dim=-1)
 
-            # original mask usage is wrong, we should not let 0 * logits, zero value logit is still large! 
-            # we use a very big negetive value replaced by logits, like -1e9
-            # y shape: [batch_size x entity_size]
             entity_logits = y.masked_fill(~mask, -1e9)
-
             temperature = self.temperature if self.is_rl_training else 1
             entity_logits = entity_logits / temperature
             del y
-            print("entity_logits:", entity_logits) if debug else None
-            print("entity_logits.shape:", entity_logits.shape) if debug else None
 
-            # entity_probs shape: [batch_size x entity_size]
             entity_probs = self.softmax(entity_logits)
-            print("entity_probs:", entity_probs) if debug else None
-            print("entity_probs.shape:", entity_probs.shape) if debug else None
-
-            # Wenhai: If this entity_id is a EOF, end the selection
-            # Implemented in 1.05 version
             entity_id = torch.multinomial(entity_probs, 1)
-            print("entity_id:", entity_id) if debug else None
-            print("entity_id.shape:", entity_id.shape) if debug else None
 
-            # note, we add a dimension where is in the seq_one to help
-            # we concat to the one : [batch_size x max_selected x ?]
             units_logits.append(entity_logits.unsqueeze(-2))
             units.append(entity_id.unsqueeze(-2))
 
-            # AlphaStar: That entity is masked out so that it cannot be selected in future iterations.
-            mask[torch.arange(batch_size), entity_id.squeeze(dim=1)] = False
+            mask[torch.arange(batch_size), entity_id.squeeze(dim=1)] = False  # masked out so that it cannot be selected in future iterations.
 
-            # whether we select the EOF
-            # note last_index should be bool type to make sure it is a right whether mask 
             last_index = (entity_id.squeeze(dim=1) == end_index)
-
-            # We should also set the flag is_end to judge whether a sample is end selection
             is_end[last_index] = 1
-            print("is_end:", is_end) if debug else None
-            print("is_end.shape:", is_end.shape) if debug else None
 
             # we record how many items we select in a sample
             # we select i + 1 items, but this include the EOF, so actually items should be i + 1 - 1
@@ -246,35 +202,13 @@ class SelectedUnitsHead(nn.Module):
             entity_one_hot = L.tensor_one_hot(entity_id, entity_size).squeeze(-2)
             entity_one_hot_unsqueeze = entity_one_hot.unsqueeze(-2) 
 
-            # entity_one_hot_unsqueeze shape: [batch_size x seq_len x entity_size], note seq_len =1 
-            # key_shape: [batch_size x entity_size x key_size], note key_size = 32
             out = torch.bmm(entity_one_hot_unsqueeze, key).squeeze(-2)
-
-            # AlphaStar: reduced by the mean across the entities,
-            # Wenhai: should be key mean
-            # Ruo-Ze: should be out mean
-            # New: it seems that it should be the key mean
-            # key_avg shape: [batch_size x key_size]
             out = out - key_avg
-
-            # t shape: [batch_size, autoregressive_embedding_size]
             t = self.project(out)
-
-            # AlphaStar: and added to `autoregressive_embedding` for subsequent iterations.
-            # autoregressive_embedding: [batch_size x autoregressive_embedding_size]
             autoregressive_embedding = autoregressive_embedding + t * ~is_end.unsqueeze(dim=1)
             del t
-            print("autoregressive_embedding:", autoregressive_embedding) if debug else None
 
-            # QUESTION: When to break?
-            # ANSWER: If all the samples in the batch end the selection, end the iteration
             if is_end.all():
-                # Note: the original below writing is wrong !
-                # print('early break in SelectedUnitsHead!' if debug else None)
-                # which makes the if the debug is false, it will output a "None",
-                # this mistake is hard to find and be locateded. Becareful of the position of
-                # the right bracket. 
-                print('early break in SelectedUnitsHead!') if debug else None
                 break
 
         # units_logits: [batch_size x select_units x entity_size]
@@ -288,7 +222,7 @@ class SelectedUnitsHead(nn.Module):
         padding_size = self.max_selected - units_logits.shape[1]
         if padding_size > 0:
             pad_units_logits = torch.ones(units_logits.shape[0], padding_size, units_logits.shape[2],
-                                          dtype=units_logits.dtype, device=units_logits.device) * 0.  # * (-1e9)
+                                          dtype=units_logits.dtype, device=units_logits.device) * (-1e9)
             units_logits = torch.cat([units_logits, pad_units_logits], dim=1) 
 
             pad_units = torch.zeros(units.shape[0], padding_size, units.shape[2],
@@ -308,7 +242,7 @@ class SelectedUnitsHead(nn.Module):
 
         select_units_num[no_select_units_index] = 0
         autoregressive_embedding[no_select_units_index] = original_ae[no_select_units_index]
-        units_logits[no_select_units_index] = 0.  # -1e9  # a magic number
+        units_logits[no_select_units_index] = -1e9  # a magic number
         units[no_select_units_index, :, 0] = entity_size - 1  # None index, the same as -1
 
         print("select_units_num:", select_units_num) if debug else None
@@ -318,7 +252,8 @@ class SelectedUnitsHead(nn.Module):
 
         return units_logits, units, autoregressive_embedding, select_units_num
 
-    def mimic_forward(self, autoregressive_embedding, action_type, entity_embeddings, entity_num, units, select_units_num):
+    def mimic_forward(self, autoregressive_embedding, action_type, entity_embeddings, entity_num, units, select_units_num,
+                      show=False):
         '''
         Inputs:
             autoregressive_embedding: [batch_size x autoregressive_embedding_size]
@@ -399,8 +334,6 @@ class SelectedUnitsHead(nn.Module):
         # calculate the average of keys (consider the entity_num)
         key_mask = mask.unsqueeze(dim=2).repeat(1, 1, key.shape[-1])
         key_avg = torch.sum(key * key_mask, dim=1) / entity_num.reshape(batch_size, 1)
-        print("key_avg:", key_avg) if debug else None
-        print("key_avg.shape:", key_avg.shape) if debug else None
 
         # creates a new variable corresponding to ending unit selection.
         # QUESTION: how to do that?
@@ -413,7 +346,6 @@ class SelectedUnitsHead(nn.Module):
 
         # designed with reference to DI-star
         max_seq_len = select_units_num.max()
-        print("max_seq_len:", max_seq_len) if debug else None
 
         # for select_units_num
         selected_mask = torch.arange(max_seq_len, device=device).float()
@@ -423,11 +355,10 @@ class SelectedUnitsHead(nn.Module):
         selected_mask = selected_mask < select_units_num.unsqueeze(dim=1)
         assert selected_mask.dtype == torch.bool
 
-        print("selected_mask:", selected_mask) if debug else None
-        print("selected_mask.shape:", selected_mask.shape) if debug else None
-
         # in the first selection, we should not select the end_index
         mask[torch.arange(batch_size), end_index] = False
+
+        is_end = torch.zeros(batch_size, device=device).bool()
 
         # designed with reference to DI-star
         for i in range(max_seq_len):
@@ -437,8 +368,6 @@ class SelectedUnitsHead(nn.Module):
                     mask[torch.arange(batch_size), end_index] = True
 
             # AlphaStar: the network passes `autoregressive_embedding` through a linear of size 256,
-            # autoregressive_embedding shape: [batch_size x autoregressive_embedding_size]
-            # x shape: [batch_size x 256]
             x = self.fc_1(autoregressive_embedding)
 
             # AlphaStar: adds `func_embed`, and passes the combination through a ReLU and a linear of size 32.
@@ -447,12 +376,6 @@ class SelectedUnitsHead(nn.Module):
 
             # AlphaStar: The result is fed into a LSTM with size 32 and zero initial state to get a query.
             query, hidden = self.small_lstm(x, hidden)
-
-            # AlphaStar: The entity keys are multiplied by the query, and are sampled using the mask and temperature 0.8 
-            # to decide which entity to select.
-            # key_shape: [batch_size x entity_size x key_size], note key_size = 32
-            # query_shape: [batch_size x seq_len x hidden_size], note hidden_size is also 32, seq_len = 1
-            # y shape: [batch_size x entity_size]
             y = torch.sum(query * key, dim=-1)
 
             # original mask usage is wrong, we should not let 0 * logits, zero value logit is still large! 
@@ -463,8 +386,6 @@ class SelectedUnitsHead(nn.Module):
             temperature = self.temperature if self.is_rl_training else 1
             entity_logits = entity_logits / temperature
             del y
-            print('entity_logits', entity_logits) if debug else None
-            print('entity_logits.shape', entity_logits.shape) if debug else None
 
             # note, we add a dimension where is in the seq_one to help
             # we concat to the one : [batch_size x max_selected x ?]
@@ -474,11 +395,15 @@ class SelectedUnitsHead(nn.Module):
             if i != max_seq_len - 1:
 
                 entity_id = units[:, i]
-                print('entity_id', entity_id) if debug else None
-                print('entity_id.shape', entity_id.shape) if debug else None
+                print('entity_id', entity_id[0]) if show else None
+                print('entity_id.shape', entity_id[0].shape) if show else None
+
+                last_index = (entity_id.squeeze(dim=1) == end_index)
+                is_end[last_index] = 1
 
                 # AlphaStar: That entity is masked out so that it cannot be selected in future iterations.
                 mask[torch.arange(batch_size), entity_id.squeeze(dim=1)] = False
+                print('mask', mask[0]) if show else None
 
                 # AlphaStar: The one-hot position of the selected entity is multiplied by the keys, 
                 # reduced by the mean across the entities, passed through a linear layer of size 1024, 
@@ -491,19 +416,11 @@ class SelectedUnitsHead(nn.Module):
                 out = torch.bmm(entity_one_hot_unsqueeze, key).squeeze(-2)
 
                 # AlphaStar: reduced by the mean across the entities,
-                # Wenhai: should be key mean
-                # Ruo-Ze: should be out mean
-                # New: it seems that it should be the key mean
-                # key_avg shape: [batch_size x key_size]
                 out = out - key_avg
 
                 # t shape: [batch_size, autoregressive_embedding_size]
                 t = self.project(out)
 
-                # AlphaStar: and added to `autoregressive_embedding` for subsequent iterations.
-                # autoregressive_embedding: [batch_size x autoregressive_embedding_size]
-                # note, here should be select_mask not ~select_mask! Fix this make the selected_units_num_right
-                # go from 0.02 to 0.25！
                 # TODO, whether should be select_mask[:, i + 1] or select_mask[:, i] ?
                 autoregressive_embedding = autoregressive_embedding + t * selected_mask[:, i + 1].unsqueeze(dim=1)
                 del t

alphastarmini/core/rl/learner.py
@@ -45,7 +45,7 @@ SAVE_PATH = os.path.join(MODEL_PATH, MODEL + "_" + strftime("%y-%m-%d_%H-%M-%S",
 class Learner:
     """Learner worker that updates agent parameters based on trajectories."""
 
-    def __init__(self, player, optimizer, global_model, 
+    def __init__(self, player, rank, cuda_device, optimizer, global_model, 
                  max_time_for_training=60 * 3, lr=THP.learning_rate, 
                  weight_decay=THP.weight_decay, baseline_weight=1,
                  is_training=True, buffer_lock=None, writer=None,
@@ -53,12 +53,19 @@ class Learner:
                  num_epochs=THP.num_epochs, count_of_batches=1,
                  buffer_size=10, use_random_sample=False,
                  only_update_baseline=False,
-                 need_save_result=True, process_lock=None):
+                 need_save_result=True, process_lock=None,
+                 update_params_interval=10):
         self.player = player
         self.player.set_learner(self)
+
+        self.rank = rank
+        self.cuda_device = cuda_device
+
         self.trajectories = []
         self.final_trajectories = []
         self.win_trajectories = []
+
+        self.update_params_interval = update_params_interval
 
         # PyTorch code
         # self.optimizer = Adam(self.get_parameters(), 
@@ -196,49 +203,71 @@ class Learner:
         agent = self.player.agent
         batch_size = AHP.batch_size
 
-        # test mixed trajectories, it does not well
-        # trajectories = self.get_mixed_trajectories()
+        learner_name = 'learner_' + str(self.rank)
 
-        print('len(self.trajectories)', len(self.trajectories)) if 1 else None
+        print(learner_name, 'len(self.trajectories)', len(self.trajectories)) if 1 else None
 
         trajectories = self.get_normal_trajectories()
-        print('len(trajectories)', len(trajectories)) if 1 else None
+        print(learner_name, 'len(trajectories)', len(trajectories)) if debug else None
 
         # agent.agent_nn.model.train()  # for BN and dropout
-        print("begin backward") if 1 else None
+        print(learner_name, "begin rl update") if debug else None
 
         for ep_id in range(self.num_epochs):
 
             for batch_id in range(self.count_of_batches):
                 update_trajectories = trajectories[batch_id * batch_size: (batch_id + 1) * batch_size]
-                print('len(update_trajectories)', len(update_trajectories)) if 1 else None
+                print(learner_name, 'len(update_trajectories)', len(update_trajectories)) if debug else None
 
-                # with torch.autograd.set_detect_anomaly(True):
-                loss, loss_dict = loss_function(agent, update_trajectories, self.use_opponent_state, 
-                                                self.no_replay_learn, self.only_update_baseline,
-                                                self.baseline_weight)
-                loss_dict_items = loss_dict.items()
-                loss_item = loss.item()
+                loss_dict_items = None
+                loss_item = None
 
                 with self.process_lock:
-                    self.optimizer.zero_grad()
+                    print(learner_name, "begin update") if debug else None
+                    SA.show_datas(agent.agent_nn.model, self.global_model, debug)
+                    SA.show_grads(agent.agent_nn.model, self.global_model, debug)
 
-                    loss.backward()
-
-                    SA.ensure_shared_grads(agent.agent_nn.model, self.global_model)
-
-                    self.optimizer.step()
-
+                    print(learner_name, "begin synchronize") if debug else None
                     agent.agent_nn.model.load_state_dict(self.global_model.state_dict())
+                    SA.show_datas(agent.agent_nn.model, self.global_model, debug)
 
-                del loss, loss_dict, update_trajectories
-                print("loss:", loss_item) if 1 else None
+                    loss, loss_dict = loss_function(agent, update_trajectories, self.use_opponent_state, 
+                                                    self.no_replay_learn, self.only_update_baseline,
+                                                    self.baseline_weight)
+                    loss_dict_items = loss_dict.items()
+                    loss_item = loss.item()
+
+                    print(learner_name, "begin zero_grad") if debug else None
+                    self.optimizer.zero_grad()
+                    SA.show_grads(agent.agent_nn.model, self.global_model, debug)
+
+                    print(learner_name, "begin backward") if debug else None
+                    loss.backward()
+                    SA.show_grads(agent.agent_nn.model, self.global_model, debug)
+
+                    print(learner_name, "begin shared_grads") if debug else None
+                    SA.ensure_shared_grads(agent.agent_nn.model, self.global_model, debug)
+                    SA.show_grads(agent.agent_nn.model, self.global_model, debug)
+
+                    print(learner_name, "begin optimizer_step") if debug else None
+                    self.optimizer.step()
+                    SA.show_datas(agent.agent_nn.model, self.global_model, debug)
+
+                    print(learner_name, "begin load_state_dict") if debug else None
+                    agent.agent_nn.model.load_state_dict(self.global_model.state_dict())
+                    SA.show_datas(agent.agent_nn.model, self.global_model, debug)
+
+                    del loss, loss_dict
+                    print(learner_name, "end update") if debug else None
+
+                del update_trajectories
+                print(learner_name, "loss:", loss_item) if debug else None
 
                 if self.need_save_result:
-                    self.writer.add_scalar('learner/loss', loss_item, agent.steps)
+                    self.writer.add_scalar('loss/' + learner_name, loss_item, agent.steps)
                     for i, k in loss_dict_items:
-                        print(i, k) if 1 else None
-                        self.writer.add_scalar('learner/' + i, k, agent.steps)
+                        print(i, k) if debug else None
+                        self.writer.add_scalar(learner_name + '/' + i, k, agent.steps)
 
                 agent.steps += AHP.batch_size * AHP.sequence_length
 
@@ -248,7 +277,7 @@ class Learner:
             torch.save(agent.agent_nn.model.state_dict(), SAVE_PATH + "" + ".pth")
 
         # agent.agent_nn.model.eval()
-        print("end backward") if 1 else None
+        print(learner_name, "end rl update") if debug else None
 
     def start(self):
         self.thread.start()
@@ -257,6 +286,7 @@ class Learner:
     def run(self):
         try:
             start_time = time()
+            update_params_timer = time()
             self.is_running = True
 
             while time() - start_time < self.max_time_for_training:
@@ -274,6 +304,10 @@ class Learner:
 
                     if actor_is_running:
                         print('learner trajectories size:', len(self.trajectories)) if debug else None
+
+                        if time() - update_params_timer > self.update_params_interval:
+                            self.player.agent.agent_nn.model.load_state_dict(self.global_model.state_dict())
+                            update_params_timer = time()
 
                         if len(self.trajectories) >= self.buffer_size * self.count_of_batches * AHP.batch_size:
                             print("learner begin to update parameters") if debug else None

alphastarmini/core/rl/rl_algo.py
@@ -333,7 +333,7 @@ def upgo_returns(values, rewards, discounts, bootstrap, debug=False):
 
 
 def entropy(all_logits, selected_mask=None, entity_mask=None, unit_type_entity_mask=None, 
-            outlier_remove=True):
+            outlier_remove=False):
     [policy_logits] = all_logits
     log_policy = F.log_softmax(policy_logits, dim=-1) 
     policy = torch.exp(log_policy)  # more stable than softmax(policy_logits)
@@ -346,8 +346,21 @@ def entropy(all_logits, selected_mask=None, entity_mask=None, unit_type_entity_m
     if unit_type_entity_mask is not None:
         x = x * unit_type_entity_mask
 
-    x = remove_outlier(x, outlier_remove)
     x = -policy * x
+
+    # if len(x.shape) == 3:
+    #     print('policy[0, 1, 44]', policy[0, 1, 44])
+    #     print('x[0, 1, 44]', x[0, 1, 44])
+    #     print('policy[2, 1, 40]', policy[2, 1, 40])
+    #     print('x[2, 1, 40]', x[2, 1, 40])
+
+    x = remove_outlier(x, outlier_remove)
+
+    # if len(x.shape) == 3:
+    #     print(stop)
+
+    #     print('entropy x', x)
+    #     print('x.shape', x.shape)
 
     del all_logits, policy_logits, log_policy, policy
     del selected_mask, entity_mask, unit_type_entity_mask
@@ -356,12 +369,24 @@ def entropy(all_logits, selected_mask=None, entity_mask=None, unit_type_entity_m
 
 
 def kl(all_logits, selected_mask=None, entity_mask=None, 
-       unit_type_entity_mask=None, outlier_remove=True):
+       unit_type_entity_mask=None, outlier_remove=False):
     [student_logits, teacher_logits] = all_logits
     s_logprobs = F.log_softmax(student_logits, dim=-1)
     t_logprobs = F.log_softmax(teacher_logits, dim=-1)
     teacher_probs = torch.exp(t_logprobs)  # more stable than softmax(teacher_logits)
     x = t_logprobs - s_logprobs
+
+    # if len(x.shape) == 3:
+    #     print('x[0, 1, 44]', x[0, 1, 44])
+    #     print('t_logprobs[0, 1, 44]', t_logprobs[0, 1, 44])
+    #     print('s_logprobs[0, 1, 44]', s_logprobs[0, 1, 44])
+    #     print('student_logits[0, 1, 44]', student_logits[0, 1, 44])
+    #     print('teacher_logits[0, 1, 44]', teacher_logits[0, 1, 44])
+
+    #     print('x[2, 1, 40]', x[2, 1, 40])
+
+    # print('kl x', x)
+    # print('x.shape', x.shape)
 
     if selected_mask is not None:
         x = x * selected_mask.unsqueeze(-1) 
@@ -390,7 +415,13 @@ def remove_outlier(x, remove=False):
     else:
         if outlier_mask.any() > 0:
             index = outlier_mask.nonzero(as_tuple=True)
+
+            print("index:", index) if 1 else None
+
             print("x[index]:", x[index]) if 1 else None
+
+            print(stop)
+
             del index
 
     del outlier_mask
@@ -398,14 +429,27 @@ def remove_outlier(x, remove=False):
     return x
 
 
-def log_prob(logits, actions, mask_used, max_selected, outlier_remove=True):
+def log_prob(logits, actions, mask_used, max_selected, show=False, outlier_remove=False):
     """Returns the log probability of taking an action given the logits."""
     [selected_mask, entity_mask, unit_type_entity_mask] = mask_used
 
+    # print('logits.shape', logits.shape) if show else None
+    # print('logits[0][0]', logits[0][0]) if show else None
+    # print('logits[0][1]', logits[0][1]) if show else None
+
+    # print('actions.shape', actions.shape) if show else None
+    # print('actions[0][0]', actions[0][0]) if show else None
+    # print('actions[0][1]', actions[0][1]) if show else None
+
     if unit_type_entity_mask is not None:
         unit_type_entity_mask = unit_type_entity_mask.reshape(-1, unit_type_entity_mask.shape[-1])
+        # print('unit_type_entity_mask[0]', unit_type_entity_mask[0]) if show else None
+        # print('unit_type_entity_mask.shape', unit_type_entity_mask.shape) if show else None
+
     if entity_mask is not None:
         entity_mask = entity_mask.reshape(-1, entity_mask.shape[-1])
+        # print('entity_mask[0]', entity_mask[0]) if show else None
+        # print('entity_mask.shape', entity_mask.shape) if show else None
 
     if len(logits.shape) == 3:
         select_size = logits.shape[1]
@@ -416,40 +460,111 @@ def log_prob(logits, actions, mask_used, max_selected, outlier_remove=True):
             entity_mask = entity_mask.unsqueeze(1).repeat(1, select_size, 1)
             unit_type_entity_mask = unit_type_entity_mask.view(-1, unit_type_entity_mask.shape[-1])
             entity_mask = entity_mask.view(-1, entity_mask.shape[-1])
-
             entity_mask = entity_mask * unit_type_entity_mask
+            # print('entity_mask[0]', entity_mask[0]) if show else None
+            # print('entity_mask.shape', entity_mask.shape) if show else None
+
     if len(actions.shape) == 3:
         actions = actions.view(-1, actions.shape[-1])
 
     del unit_type_entity_mask
 
     def cross_entropy_mask_class(pred, soft_targets, mask=None):
-        x = - soft_targets * F.log_softmax(pred, dim=-1)
-        if mask is not None:
-            x = x * mask
-        x = remove_outlier(x, outlier_remove)
+        x = F.log_softmax(pred, dim=-1)
+
+        # print('x[12]', x[12]) if show else None
+        # print('x[25]', x[25]) if show else None
+        # print('x.shape', x.shape) if show else None
+
+        x = - soft_targets * x
+
+        # print('x[12]', x[12]) if show else None
+        # print('x[25]', x[25]) if show else None
+        # print('x.shape', x.shape) if show else None
+
+        # if mask is not None:
+        #     x = x * mask
+
+        # print('mask[0]', mask[0]) if show else None
+        # print('mask[1]', mask[1]) if show else None
+        # print('mask.shape', mask.shape) if show else None
+        # print('x[0]', x[0]) if show else None
+        # print('x[1]', x[1]) if show else None
+        # print('x.shape', x.shape) if show else None
+
+        # print('x[0]', x[0]) if show else None
+        # print('x.shape', x.shape) if show else None        
+
         x = torch.sum(x, -1)
+        # print('x[0]', x[0]) if show else None
+        # print('x[1]', x[1]) if show else None
+        # print('x.shape', x.shape) if show else None
+
         del soft_targets, pred
+
         return x
+
+    # print('logits', logits[0]) if show else None
+    # print('logits.shape', logits.shape) if show else None
+
+    # print('actions', actions[0]) if show else None
+    # print('actions.shape', actions.shape) if show else None
 
     # actions: shape [BATCH_SIZE, 1] to [BATCH_SIZE]
     actions = torch.squeeze(actions, dim=-1)
     actions = F.one_hot(actions, num_classes=logits.shape[-1])
+
+    # print('actions[0]', actions[0]) if show else None
+    # print('actions[1]', actions[1]) if show else None
+    # print('actions.shape', actions.shape) if show else None
+
+    # print('logits[0]', logits[0]) if show else None
+    # print('logits[1]', logits[1]) if show else None
+    # print('logits.shape', logits.shape) if show else None
+
     loss_result = cross_entropy_mask_class(logits, actions, entity_mask)
 
     del logits, actions, entity_mask
 
     if selected_mask is not None:
         selected_mask = selected_mask.view(-1, selected_mask.shape[-1])
+
+        selected_mask_index = torch.sum(selected_mask.float(), dim=-1)
+        print('selected_mask_index', selected_mask_index) if show else None
+
+        select_index = selected_mask_index > 2
+
         if len(loss_result.shape) == 1:
             loss_result = loss_result.view(-1, max_selected)
 
+            #print(stop) if show else None
+
+        # print('loss_result[0]', loss_result[0]) if show else None
+        # print('loss_result.shape', loss_result.shape) if show else None    
+
         loss_result = loss_result * selected_mask
+
+        print('selected_mask[0]', selected_mask[0]) if show else None
+        print('selected_mask.shape', selected_mask.shape) if show else None
+
+        loss_result_2 = loss_result[select_index]
+        if len(loss_result_2):
+            print('loss_result_2', torch.sum(loss_result_2, dim=-1)) if show else None
+
+        loss_result = remove_outlier(loss_result, outlier_remove)
+
         loss_result = torch.sum(loss_result, dim=-1)
+
+        print('loss_result', loss_result) if show else None
 
         del selected_mask
 
-    return -loss_result
+    loss_result = -loss_result
+
+    # print('loss_result', loss_result[0]) if show else None
+    # print('loss_result.shape', loss_result.shape) if show else None
+
+    return loss_result
 
 
 def test(debug=True):
@@ -501,8 +616,8 @@ def test(debug=True):
 
             returns[-1, :] = rewards_short[-1, :] + discounts[-1, :] * boostrapvales[-1, :]
             for t in reversed(range(rewards_short.shape[0] - 1)):
-                returns[t, :] = rewards_short[t, :] + discounts[t, :] * (lambdas * returns[t + 1, :] +
-                                                                         (1 - lambdas) * boostrapvales[t, :])
+                returns[t, :] = rewards_short[t, :] + discounts[t, :] * (lambdas * returns[t + 1, :]
+                                                                         + (1 - lambdas) * boostrapvales[t, :])
             print("returns:", returns) if debug else None
 
             result = returns.detach() - baselines[:-1]

alphastarmini/core/rl/rl_loss.py
@@ -256,7 +256,7 @@ def sum_vtrace_loss(target_logits_all, trajectories, baselines, rewards, selecte
     mask_provided = [selected_mask, entity_mask, unit_type_entity_mask]
 
     #fields_weight = [0, 0, 0, 0, 0, 0]
-    fields_weight = [1, 0, 1, 0, 1, 1]
+    fields_weight = [0, 0, 0, 1, 0, 0]
 
     loss = 0.
     for i, field in enumerate(ACTION_FIELDS):
@@ -266,7 +266,7 @@ def sum_vtrace_loss(target_logits_all, trajectories, baselines, rewards, selecte
 
         loss_field = (-target_log_prob) * weighted_advantage * masks.reshape(-1)
         loss_field = loss_field.mean()
-        print('field', field, 'loss_val', loss_field.item()) if debug else None
+        print('field', field, 'loss_val', loss_field.item()) if 1 else None
 
         loss = loss + loss_field * fields_weight[i]
         del loss_field, weighted_advantage, target_log_prob, clipped_rhos, masks
@@ -292,7 +292,7 @@ def sum_upgo_loss(target_logits_all, trajectories, baselines, selected_mask, ent
     mask_provided = [selected_mask, entity_mask, unit_type_entity_mask]
 
     #fields_weight = [0, 0, 0, 0, 0, 0]
-    fields_weight = [1, 0, 1, 0, 1, 1]
+    fields_weight = [0, 0, 0, 0, 0, 0]
 
     loss = 0.
     for i, field in enumerate(ACTION_FIELDS):
@@ -323,7 +323,15 @@ def change_units_and_logits(behavior_logits, gt_units, select_units_num, entity_
     select_units_num = select_units_num.reshape(-1).long()
     entity_nums = entity_nums.reshape(-1).long()
 
-    behavior_logits[torch.arange(batch_size), select_units_num] = L.tensor_one_hot(entity_nums, units_size).float()
+    # print('behavior_logits[0][1]', behavior_logits[0][1]) if 1 else None
+    # print('behavior_logits.shape', behavior_logits.shape) if 1 else None
+
+    behavior_logits[torch.arange(batch_size), -1] = (L.tensor_one_hot(entity_nums, units_size).float() - 1) * 1e9
+
+    # print('behavior_logits[0][1]', behavior_logits[0][1]) if 1 else None
+    # print('behavior_logits[0][-1]', behavior_logits[0][-1]) if 1 else None
+    # print('behavior_logits.shape', behavior_logits.shape) if 1 else None
+
     gt_units = gt_units.long()
 
     padding = torch.zeros(batch_size, 1, 1, dtype=gt_units.dtype, device=gt_units.device)
@@ -331,7 +339,17 @@ def change_units_and_logits(behavior_logits, gt_units, select_units_num, entity_
     padding[:, 0] = token
 
     gt_units = torch.cat([gt_units, padding], dim=1)
-    gt_units[torch.arange(batch_size), select_units_num] = entity_nums.unsqueeze(dim=1)
+
+    # print('gt_units[0][1]', gt_units[0][1]) if 1 else None
+    # print('gt_units.shape', gt_units.shape) if 1 else None
+
+    gt_units[torch.arange(batch_size), -1] = entity_nums.unsqueeze(dim=1)
+
+    # print('gt_units[0][1]', gt_units[0][1]) if 1 else None
+    # print('gt_units.shape', gt_units.shape) if 1 else None
+
+    # print('stop', stop)
+
     gt_units = gt_units.long()
 
     return behavior_logits, gt_units
@@ -353,6 +371,7 @@ def get_logprob_and_rhos(target_logits_all, field, trajectories, mask_provided):
     max_selected = AHP.max_selected + 1
 
     all_logits = [target_logits, behavior_logits]
+    show = False
     if field == "units":
         select_units_num = torch.tensor(trajectories.player_select_units_num, dtype=torch.float32, device=device)
         entity_num = torch.tensor(trajectories.entity_num, dtype=torch.float32, device=device)
@@ -360,6 +379,7 @@ def get_logprob_and_rhos(target_logits_all, field, trajectories, mask_provided):
         all_logits = [target_logits, modified_behavior_logits]
         all_logits = [logit.reshape(sequence_length * batch_size, max_selected, AHP.max_entities) for logit in all_logits]
         mask_used = [selected_mask, entity_mask, unit_type_entity_mask]
+        show = True
     elif field == "target_unit":
         all_logits = [logit.reshape(sequence_length * batch_size, 1, AHP.max_entities) for logit in all_logits]
         mask_used = [None, entity_mask, None]
@@ -374,13 +394,23 @@ def get_logprob_and_rhos(target_logits_all, field, trajectories, mask_provided):
 
     [target_logits, behavior_logits] = all_logits
 
-    target_log_prob = RA.log_prob(target_logits, actions, mask_used, max_selected)
-    behavior_log_prob = RA.log_prob(behavior_logits, actions, mask_used, max_selected)
+    target_log_prob = RA.log_prob(target_logits, actions, mask_used, max_selected, show=show)
+    behavior_log_prob = RA.log_prob(behavior_logits, actions, mask_used, max_selected, show=show)
+
+    # print('target_log_prob', target_log_prob) if show else None
+    # print('target_log_prob.shape', target_log_prob.shape) if show else None
+
+    # print('behavior_log_prob', behavior_log_prob) if show else None
+    # print('behavior_log_prob.shape', behavior_log_prob.shape) if show else None
+
     with torch.no_grad():
         clipped_rhos = RA.compute_cliped_importance_weights(target_log_prob, behavior_log_prob)
 
     if field == 'units':
         clipped_rhos = clipped_rhos.reshape(seqbatch_shape, -1).sum(dim=-1)
+        # print('clipped_rhos', clipped_rhos) if show else None
+        # print('clipped_rhos.shape', clipped_rhos.shape) if show else None
+
     clipped_rhos = clipped_rhos.reshape(sequence_length, batch_size)
 
     del behavior_log_prob, target_logits, behavior_logits, all_logits, target_logits_all
@@ -391,11 +421,13 @@ def get_logprob_and_rhos(target_logits_all, field, trajectories, mask_provided):
 
 def loss_function(agent, trajectories, use_opponent_state=True, 
                   no_replay_learn=False, only_update_baseline=False,
-                  learner_baseline_weight=1):
+                  learner_baseline_weight=1, show=False):
     """Computes the loss of trajectories given weights."""
 
     # target_logits: ArgsActionLogits
-    target_logits, baselines, target_select_units_num, target_entity_num = agent.rl_unroll(trajectories, use_opponent_state)
+    target_logits, baselines, select_units_num, entity_num = agent.rl_unroll(trajectories, 
+                                                                             use_opponent_state, 
+                                                                             show=show)
     device = target_logits.action_type.device
 
     # transpose to [seq_size x batch_size x -1]
@@ -403,11 +435,11 @@ def loss_function(agent, trajectories, use_opponent_state=True,
     baselines = transpose_baselines(baselines)
 
     # transpose to [seq_size x batch_size x -1]
-    target_select_units_num = transpose_sth(target_select_units_num)
-    target_entity_num = transpose_sth(target_entity_num)
+    select_units_num = transpose_sth(select_units_num)
+    entity_num = transpose_sth(entity_num)
 
     # get used masks
-    selected_mask, entity_mask = get_useful_masks(target_select_units_num, target_entity_num, device)
+    selected_mask, entity_mask = get_useful_masks(select_units_num, entity_num, device)
 
     # note, we change the structure of the trajectories
     # shape before: [dict_name x batch_size x seq_size]
@@ -437,7 +469,8 @@ def loss_function(agent, trajectories, use_opponent_state=True,
         print("reward_name:", reward_name) if debug else None
 
         rewards = PR.compute_pseudoreward(trajectories, reward_name, device)
-        print("rewards:", rewards) if debug else None
+        print("rewards:", rewards) if 1 else None
+        print("rewards not 0:", rewards[rewards != 0]) if 1 else None
 
         # The action_type argument, delay, and all other arguments are separately updated 
         # using a separate ("split") VTrace Actor-Critic losses. 

alphastarmini/core/rl/rl_vs_inner_bot_mp.py
@@ -16,6 +16,7 @@ import numpy as np
 
 import torch
 import torch.multiprocessing as mp
+import torch.nn.functional as F
 
 from tensorboardX import SummaryWriter
 
@@ -50,15 +51,15 @@ speed = False
 
 SIMPLE_TEST = not P.on_server
 if SIMPLE_TEST:
-    MAX_EPISODES = 5
+    MAX_EPISODES = 1
     ACTOR_NUMS = 1
-    PARALLEL = 2
-    GAME_STEPS_PER_EPISODE = 500  
-    MAX_FRAMES = 500 * 5
+    PARALLEL = 1
+    GAME_STEPS_PER_EPISODE = 18000  
+    MAX_FRAMES = 18000 * 5
 else:
-    MAX_EPISODES = 4 * 12
-    ACTOR_NUMS = 4
-    PARALLEL = 8 + 7 * 1
+    MAX_EPISODES = 16 * 2
+    ACTOR_NUMS = 1
+    PARALLEL = 8  # + 7 * 1
     GAME_STEPS_PER_EPISODE = 18000
     MAX_FRAMES = 18000 * MAX_EPISODES
 
@@ -67,11 +68,12 @@ TRAIN_ITERS = MAX_EPISODES * ACTOR_NUMS * PARALLEL
 
 USE_UPDATE_LOCK = True
 DIFFICULTY = 2
-ONLY_UPDATE_BASELINE = False
+ONLY_UPDATE_BASELINE = True
 BASELINE_WEIGHT = 1
-LR = 1e-5  # 1e-5
+LR = 1e-4  # 1e-5
 WEIGHT_DECAY = 0
 
+USE_MIDDLE_REWARD = True
 USE_DEFINED_REWARD_AS_REWARD = False
 USE_RESULT_REWARD = True
 REWARD_SCALE = 1e-3
@@ -133,7 +135,8 @@ class ActorVSComputer:
     We don't use batched inference here, but it was used in practice.
     """
 
-    def __init__(self, player, queue, device, coordinator, teacher, idx, buffer_lock=None, results_lock=None, 
+    def __init__(self, player, queue, device, global_model, coordinator, 
+                 teacher, idx, buffer_lock=None, results_lock=None, 
                  writer=None, max_time_for_training=60 * 60 * 24,
                  max_time_per_one_opponent=60 * 60 * 4,
                  max_frames_per_episode=22.4 * 60 * 15, max_frames=MAX_FRAMES, 
@@ -146,6 +149,7 @@ class ActorVSComputer:
         self.idx = idx
         self.teacher = teacher
         self.queue = queue
+        self.global_model = global_model
         self.coordinator = coordinator
 
         self.agent = self.player.agent
@@ -270,6 +274,7 @@ class ActorVSComputer:
                                 # if time() - update_params_timer > self.update_params_interval:
                                 #     print("agent_{:d} update params".format(self.idx)) if debug else None
                                 #     self.agent.set_weights(self.player.agent.get_weights())
+                                #     self.agent.agent_nn.model.load_state_dict(self.global_model.state_dict())
                                 #     update_params_timer = time()
 
                                 state = self.agent.agent_nn.preprocess_state_all(home_obs.observation, 
@@ -283,9 +288,21 @@ class ActorVSComputer:
 
                                 print("player_function_call:", player_function_call) if debug else None
                                 print("player_action.delay:", player_action.delay) if debug else None
+
                                 print("entity_num:", entity_num) if debug else None
                                 print("player_select_units_num:", player_select_units_num) if debug else None
                                 print("player_action:", player_action) if debug else None
+
+                                # print("player_action.units:", player_action.units) if 1 else None
+                                # print("player_logits.units:", player_logits.units) if 1 else None
+                                # print("player_logits.units:", player_logits.units.shape) if 1 else None
+
+                                # print("player_logits.units[0]:", player_logits.units[0]) if 1 else None
+                                # print("player_logits.units[0][0]:", player_logits.units[0][0]) if 1 else None
+                                # print("F.soft_max(player_logits.units[0][0]):", F.softmax(player_logits.units[0][0])) if 1 else None
+                                # print("player_logits.units[0][1]:", player_logits.units[0][1]) if 1 else None
+                                # print("F.soft_max(player_logits.units[0][1]):", F.softmax(player_logits.units[0][1])) if 1 else None
+                                # print('stop', stop)
 
                                 if False:
                                     show_sth(home_obs, player_action)
@@ -312,7 +329,11 @@ class ActorVSComputer:
                                 total_frames += 1 * STEP_MUL
                                 episode_frames += 1 * STEP_MUL
 
-                                reward = home_next_obs.reward
+                                # fix the action delay
+                                player_action.delay = torch.tensor([[STEP_MUL]], dtype=player_action.delay.dtype,
+                                                                   device=player_action.delay.device)
+
+                                reward = float(home_next_obs.reward)
                                 print("reward: ", reward) if 0 else None
 
                                 is_final = home_next_obs.last()
@@ -329,11 +350,11 @@ class ActorVSComputer:
 
                                 points = get_points(home_next_obs)
 
-                                if USE_DEFINED_REWARD_AS_REWARD:
+                                if USE_MIDDLE_REWARD:
                                     if last_points is not None:
                                         reward = points - last_points
                                     else:
-                                        reward = 0
+                                        reward = 0.
                                 last_points = points
 
                                 if is_final:
@@ -358,7 +379,7 @@ class ActorVSComputer:
                                     killed_minerals = np.sum(o['score_by_category']['killed_minerals'])
                                     killed_vespene = np.sum(o['score_by_category']['killed_vespene'])
 
-                                    killed_points = killed_minerals + killed_vespene
+                                    killed_points = float(killed_minerals + killed_vespene)
 
                                     if game_outcome == 1:
                                         outcome = 1
@@ -378,7 +399,7 @@ class ActorVSComputer:
                                         outcome = -1
 
                                     if not USE_DEFINED_REWARD_AS_REWARD:
-                                        reward = outcome
+                                        reward = float(outcome)
                                         if outcome == 0:
                                             reward = killed_points / float(WIN_THRESHOLD)
                                             with self.results_lock:
@@ -386,7 +407,7 @@ class ActorVSComputer:
                                                 print("agent_{:d} get final game_outcome".format(self.idx), game_outcome) if 1 else None
                                                 print("agent_{:d} get final outcome".format(self.idx), outcome) if 1 else None
                                                 print("agent_{:d} get reward".format(self.idx), reward) if 1 else None
-                                                print("agent_{:d} get reward_2".format(self.idx), (killed_points - 1000) / 3000.0) if 1 else None
+                                                print("agent_{:d} get reward_2".format(self.idx), killed_points / float(WIN_THRESHOLD)) if 1 else None
 
                                     food_used_list.append(food_used)
                                     army_count_list.append(army_count)
@@ -399,7 +420,7 @@ class ActorVSComputer:
                                     print("agent_{:d} get final reward".format(self.idx), reward) if 1 else None
                                     print("agent_{:d} get outcome".format(self.idx), outcome) if 1 else None
 
-                                    final_points = killed_points * REWARD_SCALE
+                                    final_points = killed_points / float(WIN_THRESHOLD)
                                     if self.need_save_result:
                                         self.writer.add_scalar('final_points/' + 'agent_' + str(self.idx), final_points, total_episodes)
                                         with self.results_lock:
@@ -417,7 +438,7 @@ class ActorVSComputer:
                                     if outcome == 1:
                                         is_win_trajectory = True 
                                 else:
-                                    print("agent_{:d} get reward".format(self.idx), reward) if debug else None
+                                    pass
 
                                 # note, original AlphaStar pseudo-code has some mistakes, we modified 
                                 # them here
@@ -431,6 +452,9 @@ class ActorVSComputer:
                                     player_action.to('cpu')
                                     player_select_units_num = player_select_units_num.to('cpu')
                                     entity_num = entity_num.to('cpu')
+
+                                print("agent_{:d} get reward".format(self.idx), reward) if debug else None
+                                print("player_action.delay:", player_action.delay) if debug else None
 
                                 traj_step = Trajectory(
                                     state=state,
@@ -591,11 +615,11 @@ def get_points(obs):
 
     killed_points = killed_minerals + killed_vespene
 
-    points = float(collected_points + used_points + 2 * killed_points)
+    #points = float(collected_points + used_points + 2 * killed_points)
 
-    #points = float(killed_points)
+    points = float(killed_points) / float(WIN_THRESHOLD)
 
-    points = points * REWARD_SCALE
+   # points = points * REWARD_SCALE
 
     return points
 
@@ -609,10 +633,10 @@ def Worker(synchronizer, rank, optimizer, queue, use_cuda_device, model_learner,
         print('process id:', os.getpid())
 
     if rank < 8:
-        cuda_device = "cuda:" + str(rank)
+        cuda_device = "cuda:" + str(rank) if use_cuda_device else 'cpu'
     else:
         new_rank = (rank - 8) % 7 + 1
-        cuda_device = "cuda:" + str(new_rank)
+        cuda_device = "cuda:" + str(new_rank) if use_cuda_device else 'cpu'
 
     league = League(
         initial_agents={
@@ -650,17 +674,20 @@ def Worker(synchronizer, rank, optimizer, queue, use_cuda_device, model_learner,
             player.agent.set_rl_training(IS_TRAINING)
 
             buffer_lock = threading.Lock()
-            learner = Learner(player, optimizer=optimizer, global_model=model_learner, 
+            learner = Learner(player, rank, cuda_device, optimizer=optimizer, global_model=model_learner, 
                               max_time_for_training=60 * 60 * 24 * 7, lr=LR, 
                               weight_decay=WEIGHT_DECAY, baseline_weight=BASELINE_WEIGHT, is_training=IS_TRAINING, 
                               buffer_lock=buffer_lock, writer=writer, use_opponent_state=USE_OPPONENT_STATE,
                               no_replay_learn=NO_REPLAY_LEARN, num_epochs=NUM_EPOCHS,
                               count_of_batches=COUNT_OF_BATCHES, buffer_size=BUFFER_SIZE,
                               use_random_sample=USE_RANDOM_SAMPLE, only_update_baseline=ONLY_UPDATE_BASELINE,
-                              need_save_result=NEED_SAVE_RESULT, process_lock=process_lock)
+                              need_save_result=NEED_SAVE_RESULT, process_lock=process_lock,
+                              update_params_interval=UPDATE_PARAMS_INTERVAL)
             learners.append(learner)
 
             teacher = get_supervised_agent(player.race, model_type="sl", restore=True, device=cuda_device)
+            teacher.set_rl_training(IS_TRAINING)
+
             # teacher.agent_nn.model = model_teacher
             if use_cuda_device:
                 teacher.agent_nn.model.to(cuda_device)
@@ -668,7 +695,7 @@ def Worker(synchronizer, rank, optimizer, queue, use_cuda_device, model_learner,
             for z in range(ACTOR_NUMS):
                 device = torch.device(cuda_device if use_cuda_device else "cpu")
                 agent_id = rank * ACTOR_NUMS + z
-                actor = ActorVSComputer(player, queue, device, coordinator, teacher, agent_id, buffer_lock, results_lock, writer)
+                actor = ActorVSComputer(player, queue, device, model_learner, coordinator, teacher, agent_id, buffer_lock, results_lock, writer)
                 actors.append(actor)
 
         threads = []
@@ -703,15 +730,11 @@ def Parameter_Server(synchronizer, queue, use_cuda_device, model, log_path, mode
         print('process id:', os.getpid())
 
     writer = SummaryWriter(log_path)
-
     torch.manual_seed(RANDOM_SEED)
 
     update_counter = 0
-    max_win_rate = 0.
-    latest_win_rate = 0.
-    win_rate_list = []
-    result_list = []
-    win_num = 0
+    max_win_rate, latest_win_rate = 0., 0.
+    win_rate_list, result_list = [], []
 
     train_iters = TRAIN_ITERS 
     static_num = STATIC_NUM  # WINRATE_SCALE * ACTOR_NUMS * PARALLEL
@@ -720,14 +743,8 @@ def Parameter_Server(synchronizer, queue, use_cuda_device, model, log_path, mode
 
     try: 
         while update_counter < train_iters:
-
-            sleep(1)
             result = queue.get(timeout=60 * 30)
-
             result_list.append(result)
-            if result == 1:
-                win_num += 1
-
             print("Parameter_Server result_list", result_list) if debug else None
 
             row = int(update_counter / static_num)
@@ -744,10 +761,13 @@ def Parameter_Server(synchronizer, queue, use_cuda_device, model, log_path, mode
                 writer.add_scalar('all_winrate', win_rate, row + 1)
                 win_rate_list.append(win_rate)
 
-                if win_rate > max_win_rate:
+                if win_rate >= max_win_rate:
                     with synchronizer:
                         torch.save(model.state_dict(), model_path + ".pth")
                     max_win_rate = win_rate
+                elif ONLY_UPDATE_BASELINE:
+                    with synchronizer:
+                        torch.save(model.state_dict(), model_path + ".pth")
 
                 latest_win_rate = win_rate
 
@@ -809,16 +829,6 @@ def test(on_server=False, replay_path=None):
                                             eps=THP.epsilon, weight_decay=WEIGHT_DECAY)
         optimizer.share_memory()
 
-    # device_teacher = torch.device("cuda:1" if use_cuda_device else "cpu")
-    # teacher = get_supervised_agent(player.race, model_type="sl", 
-    #                                restore=True, device=device_teacher)
-    # if ON_GPU:
-    #     teacher.agent_nn.to(device_teacher)
-
-    # model_teacher = teacher.agent_nn.model
-    # model_teacher.share_memory()
-
-    #synchronizer = mp.Barrier(PARALLEL + 1)
     synchronizer = mp.Lock()
     processes = []
     q = mp.Queue(maxsize=256 * 24)